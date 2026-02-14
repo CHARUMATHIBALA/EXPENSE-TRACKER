8d3167fcd876aa2b6f1d4a769f79afd8d5293b7d
CHARUMATHIBALA <155135192+CHARUMATHIBALA@users.noreply.github.com>
2026-02-14 11:43:22 +0530

    now

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // Enable CORS
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5175',
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 
